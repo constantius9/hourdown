ab2d5f50a8214de21b40175c285492ed1edecc1b
Michael Pankov <constantius9@gmail.com>
2012-11-11 23:41:07 +0400

    Added the main loop

---
+++ src/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""A simple tool asking for a time and date and counting down to that moment.
+
+Currently works in interactive mode only.
+"""
+
+def main():
+    """Main loop"""
+    while True:
+        print """Choose action:
+        1 - Set time/date
+        2 - Add a project
+        3 - Journal time spending
+        """
