f126f069f52f21c60b71186670c7bbb4c3cfe91b
Michael Pankov <constantius9@gmail.com>
2012-11-17 00:24:02 +0400

    Added tests for output to stdout

--- src/test_main.py
+++ src/test_main.py
@@ -4,19 +4,46 @@
 
 import mock
 import unittest
+from StringIO
 
 import main
-import datetime as dt
+
+
+class StdoutOutputTestCase(unittest.TestCase):
+    """Tests whether the stuff is printed correctly."""
+
+    def setUp(self):
+        sys.stdout = self.captured = StringIO()
+
+    def test_show_info(self):
+        pass
+
+    def test_show_actions(self):
+        main.print_actions()
+        self.assertEquals(self.captured,
+            """Choose action:
+            1 - Set time/date
+            2 - Add a project
+            3 - Journal time spending
+            4 - Show journal
+            5 - Exit
+            """)
+
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
 
 
 class InputReactionTestCase(unittest.TestCase):
-    
+    """Tests whether the user input is handled correctly."""    
+
     @mock.patch('__builtin__.raw_input')
     def test_inquiry(self, raw_input):
         """Tests user interaction"""
 
         patcher = mock.patch('sys.stdout')
         patcher.start()
+        patcher2 = mock.patch('datetime.datetime')
+        patcher2.start()
 
         raw_input.return_value = '1'
         choice = main.inquiry(hours_left=100, date_time=dt.datetime.now())
@@ -35,6 +62,11 @@ class InputReactionTestCase(unittest.TestCase):
         self.assertEquals(choice, '5')
 
         patcher.stop()
+        patcher2.stop()
+
+
+    def test_dispatch(self):
+        pass
 
 
 if __name__ == "__main__":
