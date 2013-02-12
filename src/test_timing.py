e779094cdacf242399f5da80d78c2f4bc21a5104
Michael Pankov <constantius9@gmail.com>
2013-02-13 01:40:12 +0400

    Corrected tests.

--- src/test_timing.py
+++ src/test_timing.py
@@ -10,20 +10,20 @@ class FacilitiesTest(unittest.TestCase):
         day = Day()
         schema = '8h-2×2h:4×1h:8×30m:16×15m'
         day.factor(schema)
-        self.assertEqual(day.slots['2h'], 2)
-        self.assertEqual(day.slots['1h'], 4)
-        self.assertEqual(day.slots['30m'], 8)
-        self.assertEqual(day.slots['15m'], 16)
+        self.assertEqual(len(day.slots['2h']), 2)
+        self.assertEqual(len(day.slots['1h']), 4)
+        self.assertEqual(len(day.slots['30m']), 8)
+        self.assertEqual(len(day.slots['15m']), 16)
         self.assertEqual(day.sleep, '8h')
 
     def testFactorADayContractedScheme(self):
         day = Day()
         schema = '2×2h:4×1h:8×30m:16×15m'
         day.factor(schema)
-        self.assertEqual(day.slots['2h'], 2)
-        self.assertEqual(day.slots['1h'], 4)
-        self.assertEqual(day.slots['30m'], 8)
-        self.assertEqual(day.slots['15m'], 16)
+        self.assertEqual(len(day.slots['2h']), 2)
+        self.assertEqual(len(day.slots['1h']), 4)
+        self.assertEqual(len(day.slots['30m']), 8)
+        self.assertEqual(len(day.slots['15m']), 16)
         self.assertEqual(day.sleep, '8h')
 
 
